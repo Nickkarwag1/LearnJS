462f152dee5e210209503311966d7df91f77d69a
Maryia Dolia <maryia.dolia@intapp.com>
2021-12-04 18:55:48 +0300

    Added dataTypes constants

--- utils/isNumber.js
+++ utils/isNumber.js
@@ -1,5 +1,8 @@
-function isNumber(num){
-    return typeof num === 'number';
-}
+const DATA_TYPES = require("../constants/dataTypes");
 
+const {NUMBER} = DATA_TYPES;
+
+function isNumber(num){
+    return typeof num === NUMBER;
+}
 module.exports = isNumber;
